4c3048733ececc74702c5958d866c99f6e8a1120
Toni <antoni.rovira@gmail.com>
2015-02-17 20:59:36 +0100

    jservlet

---
+++ src/common/layer/Jservlet.java
@@ -0,0 +1,35 @@
+package common.layer;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MiServlet
+ */
+public class Jservlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    public Jservlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+                   throws ServletException, IOException {
+              	
+			ServletOutputStream out=response.getOutputStream();
+			out.println("Hola mundo, Servlet!");
+			out.flush();
+			out.close();
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+                   throws ServletException, IOException {
+
+	}
+
+}
